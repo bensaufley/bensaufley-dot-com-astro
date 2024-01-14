e3adb5b3d62e7619a0a36aeeea72b8632eabcc85
Ben Saufley <bensaufley+git@gmail.com>
2024-01-13 23:27:45 -0500

    Allow theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,27 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import mdx from '@astrojs/mdx';
+import svgr from 'vite-plugin-svgr';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), mdx(), sitemap()],
+  vite: {
+    plugins: [
+      svgr({
+        svgrOptions: {
+          jsxRuntime: 'classic-preact',
+        },
+        esbuildOptions: {
+          jsxFactory: 'h',
+          jsxImportSource: 'preact',
+          jsxFragment: 'Fragment',
+        },
+      }),
+    ],
+  },
   output: 'static',
   redirects: {
     '/blog/0': '/',
