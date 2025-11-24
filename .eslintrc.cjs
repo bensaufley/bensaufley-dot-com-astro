dcbeeef954983dcc59f5a1852bb04baba58bdbf2
Ben Saufley <bensaufley+git@gmail.com>
2025-11-23 21:46:50 -0500

    Fix MDX linting, sort imports in MDX files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,18 @@ module.exports = {
       files: ['*.mdx'],
       plugins: ['mdx'],
       extends: ['plugin:mdx/recommended'],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.mdx'],
+      },
+      rules: {
+        '@typescript-eslint/consistent-type-imports': 'off',
+        'import/prefer-default-export': 'off',
+
+        // These rules generate false positives, unfortunately
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
     },
     {
       files: ['*.astro'],
